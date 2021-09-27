49407d8e587f3379837a509e661ea5aa19c9d94e
Cryptojoint <a.hamdy.nig@gmail.com>
2021-09-27 17:23:41 +0200

    Finishing PNL Tab -updated some styles -implemented the calculation logic for PNL of short/sell trades

--- src/investment/TradeCalculator.js
+++ src/investment/TradeCalculator.js
@@ -7,7 +7,7 @@ import './TradeCalculator.css';
 
 export const TradeCalculator = () => {
 	return (
-		<Container>
+		<Container className="trade-calculator">
 			<Row>
 				<Col className="tabs-col">
 					<Button className="active">PNL</Button>
@@ -66,6 +66,7 @@ const CalculatorResult = () => {
 };
 
 const CalculatorForm = () => {
+	const [tradeType, setTradeType] = useState('long');
 	const [leverage, setleverage] = useState('');
 	const [entryPrice, setEntryPrice] = useState('');
 	const [exitPrice, setExitPrice] = useState('');
@@ -76,40 +77,92 @@ const CalculatorForm = () => {
 	const exitPriceChanged = (ev) => setExitPrice(ev.target.value);
 	const quantitiyChanged = (ev) => setQuantitiy(ev.target.value);
 
+	const toggleTradeType = (ev) => {
+		const buyBtn = document.querySelector('.btn-buy');
+		const sellBtn = document.querySelector('.btn-sell');
+
+		let _list = Array.from(ev.target.classList);
+		if (ev.target.value === 'long') {
+			if (!_list.includes('active')) {
+				sellBtn.classList.remove('active');
+				buyBtn.classList.add('active');
+			}
+		} else {
+			if (!_list.includes('active')) {
+				buyBtn.classList.remove('active');
+				sellBtn.classList.add('active');
+			}
+		}
+		setTradeType(ev.target.value);
+	};
+
 	const onCalculate = () => {
-		console.log('********************');
+		console.log(`tradeType: ${tradeType}`);
 		console.log(`leverage: ${leverage}`);
-		console.log(`entryPrice: ${entryPrice}`);
-		console.log(`exitPrice: ${exitPrice}`);
-		console.log(`quantitiy: ${quantitiy}`);
 
 		const marginDom = document.querySelector('.margin-result');
 		const pnlDom = document.querySelector('.pnl-result');
 		const roeDom = document.querySelector('.roe-result');
 
-		let margin = quantitiy / leverage;
-		let pnl = ((exitPrice - entryPrice) / entryPrice) * quantitiy;
-		let roe = ((exitPrice - entryPrice) / entryPrice) * 100 * leverage;
-		console.log(`Initial Margin: ${margin.toFixed(2)}`);
-		console.log(`PNL: ${pnl.toFixed(2)}`);
-		console.log(`ROE: ${roe.toFixed(2)}%`);
+		let margin, pnl, roe;
+
+		if (tradeType === 'long') {
+			margin = quantitiy / leverage;
+			pnl = ((exitPrice - entryPrice) / entryPrice) * quantitiy;
+			roe = ((exitPrice - entryPrice) / entryPrice) * 100 * leverage;
+		} else {
+			margin = quantitiy / leverage;
+			pnl = ((entryPrice - exitPrice) / entryPrice) * quantitiy;
+			roe = ((entryPrice - exitPrice) / entryPrice) * 100 * leverage;
+		}
+
 		marginDom.innerHTML = `${margin.toFixed(2)} USDT`;
 		pnlDom.innerHTML = `${pnl.toFixed(2)} USDT`;
 		roeDom.innerHTML = `${roe.toFixed(2)} %`;
-		console.log('********************');
+	};
+
+	const getRangeOptions = () => {
+		let content = [];
+		for (let i = 1; i <= 125; i++) {
+			/* if (i === 1) {
+				content.push(<option value={i} label={i}></option>);
+			} else if (i % 25 === 0) {
+				content.push(<option value={i} label={i}></option>);
+			} else {
+				content.push(<option value={i}></option>);
+			} */
+			content.push(<option value={i}></option>);
+		}
+		return content;
 	};
 
 	return (
 		<Container className="content-left">
 			<Row>
 				<Col>
-					<Button className="btn-buy">Long</Button>
+					<Button className="btn-buy" active value={'long'} onClick={toggleTradeType}>
+						Long
+					</Button>
 				</Col>
 				<Col>
-					<Button className="btn-sell">Short</Button>
+					<Button className="btn-sell" value={'short'} onClick={toggleTradeType}>
+						Short
+					</Button>
 				</Col>
 			</Row>
 			<Row>
+				<Col md={9}>
+					<Form.Range
+						type="range"
+						list="tickmarks"
+						className="leverage-range"
+						min={1}
+						max={125}
+						value={leverage}
+						onChange={leverageChanged}
+					/>
+					{/* <datalist id="tickmarks">{getRangeOptions()}</datalist> */}
+				</Col>
 				<Col>
 					<FloatingLabel label="Leverage">
 						<Form.Control
