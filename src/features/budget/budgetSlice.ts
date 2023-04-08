e32c46a438f1c824fbde5ad04d9654b70e994619
hamdyx <a.hamdy.nig@gmail.com>
2023-04-08 15:06:34 +0200

    refactoring budget files to typescript

--- src/features/budget/budgetSlice.ts
+++ src/features/budget/budgetSlice.ts
@@ -3,53 +3,61 @@ import {
 	createAsyncThunk,
 	createEntityAdapter,
 } from '@reduxjs/toolkit';
+import { RootState } from 'app/store';
 
 const budgetAdapter = createEntityAdapter({});
 
 const initialState = budgetAdapter.getInitialState({
 	status: 'idle',
-	error: null,
+	error: '',
 });
 
 export const fetchTrxs = createAsyncThunk('transactions/fetchTrx', async () => {
-	const response = JSON.parse(localStorage.getItem('transactions'));
+	const response = JSON.parse(localStorage.getItem('transactions') || '[]');
 	return response || [];
 });
 
 export const addNewTrx = createAsyncThunk(
 	'transactions/addNewTrx',
 	async (trx) => {
-		const allTrx = JSON.parse(localStorage.getItem('transactions')) || [];
+		const allTrx = JSON.parse(localStorage.getItem('transactions') || '') || [];
 		const updatedTrx = [...allTrx, trx];
 		localStorage.setItem('transactions', JSON.stringify(updatedTrx));
 		return trx;
 	}
 );
 
-export const updateTrx = createAsyncThunk(
-	'transactions/updateTrx',
-	async (trx) => {
-		const allTrx = JSON.parse(localStorage.getItem('transactions')) || [];
-		const updated = allTrx.find((el) => el.id === trx.id);
-		Object.assign(updated, trx);
-		localStorage.setItem('transactions', JSON.stringify(allTrx));
-		return updated;
+export const updateTrx = createAsyncThunk<
+	any,
+	any,
+	{
+		state: RootState;
 	}
-);
+>('transactions/updateTrx', async (trx) => {
+	const allTrx = JSON.parse(localStorage.getItem('transactions') || '') || [];
+	const updated = allTrx.find((el: any) => el.id === trx.id);
+	Object.assign(updated, trx);
+	localStorage.setItem('transactions', JSON.stringify(allTrx));
+	return updated;
+});
 
-export const deleteTrx = createAsyncThunk(
-	'transactions/deleteTrx',
-	async (id) => {
-		const allTrx = JSON.parse(localStorage.getItem('transactions')) || [];
-		const updatedTrx = allTrx.filter((trx) => trx.id !== id);
-		localStorage.setItem('transactions', JSON.stringify(updatedTrx));
-		return id;
+export const deleteTrx = createAsyncThunk<
+	any,
+	any,
+	{
+		state: RootState;
 	}
-);
+>('transactions/deleteTrx', async (id) => {
+	const allTrx = JSON.parse(localStorage.getItem('transactions') || '') || [];
+	const updatedTrx = allTrx.filter((trx: any) => trx.id !== id);
+	localStorage.setItem('transactions', JSON.stringify(updatedTrx));
+	return id;
+});
 
 const budgetSlice = createSlice({
 	name: 'budget',
 	initialState,
+	reducers: {},
 	extraReducers: (builder) => {
 		builder
 			// ****************************** fetchTrxs ******************************
@@ -58,7 +66,7 @@ const budgetSlice = createSlice({
 			})
 			.addCase(fetchTrxs.rejected, (state, action) => {
 				state.status = 'failed';
-				state.error = action?.error?.message;
+				state.error = action?.error?.message || 'Error fetching transactions';
 			})
 			.addCase(fetchTrxs.fulfilled, (state, action) => {
 				state.status = 'succeeded';
@@ -70,7 +78,7 @@ const budgetSlice = createSlice({
 			})
 			.addCase(addNewTrx.rejected, (state, action) => {
 				state.status = 'failed';
-				state.error = action?.error?.message;
+				state.error = action?.error?.message || 'Error creating transaction';
 			})
 			.addCase(addNewTrx.fulfilled, (state, action) => {
 				state.status = 'succeeded';
@@ -82,7 +90,7 @@ const budgetSlice = createSlice({
 			})
 			.addCase(updateTrx.rejected, (state, action) => {
 				state.status = 'failed';
-				state.error = action.error.message;
+				state.error = action.error.message || 'Error updating transaction';
 			})
 			.addCase(updateTrx.fulfilled, (state, action) => {
 				state.status = 'succeeded';
@@ -94,7 +102,7 @@ const budgetSlice = createSlice({
 			})
 			.addCase(deleteTrx.rejected, (state, action) => {
 				state.status = 'failed';
-				state.error = action.error.message;
+				state.error = action.error.message || 'Error ';
 			})
 			.addCase(deleteTrx.fulfilled, (state, action) => {
 				state.status = 'succeeded';
@@ -109,4 +117,4 @@ export const {
 	selectAll: selectAllTrx,
 	selectById: selectTrxById,
 	selectIds: selectTrxIds,
-} = budgetAdapter.getSelectors((state) => state.budget);
+} = budgetAdapter.getSelectors((state: RootState) => state.budget);
